b85caad2176ccbbe8ed1450cd0bb198f61f03acf
Rusty Burchfield <GICodeWarrior@gmail.com>
2008-02-16 01:35:20 +0000

    added comments for submission

--- src/main.cpp
+++ src/main.cpp
@@ -26,36 +26,43 @@ int main()
 	
 	World scene;
 	Material *material;
-	//Sphere *sphere1 = new Sphere(Point(0,3,-2), 1, Color(255, 0, 0));
+	vector<Point> points;
+	
+	//---------- Scene definition
+	
+	// This is the bubble
 	Sphere *sphere1 = new Sphere(Point(0,3,-2), 1, Color::BLACK);
-	//Sphere *sphere2 = new Sphere(Point(2,2,2), 1, Color(0, 0, 255));
-	Sphere *sphere2 = new Sphere(Point(2,2,2), 1, Color::WHITE);
 	material = new Phong(sphere1, &scene);
 	//material = new Onion(sphere1);
 	//material = new Quilt(sphere1);
-	//material = new Reflection(sphere1, &scene, 1.0);
+	//material = new Reflection(sphere1, &scene, 0.2);
+	//material = new Phong(material, &scene);
 	material = new Transmission(material, &scene, 1.0, 1.01);
-	//material = new Phong(material, &scene);
 	scene.add(material);
-	material = new Phong(sphere2, &scene);
-	//material = new Checker(sphere2);
+	
+	// This is the reflective sphere
+	Sphere *sphere2 = new Sphere(Point(2,2,2), 1, Color::WHITE);
+	//material = new Phong(sphere2, &scene);
+	material = new Checker(sphere2);
 	//material = new Quilt(material);
-	//material = new Phong(material, &scene);
+	material = new Phong(material, &scene);
 	material = new Reflection(material, &scene, 0.1);
 	scene.add(material);
 	
-	vector<Point> points;
+	// This is the floor
+	points.clear();
 	points.push_back(Point(-4, 0, -2));
 	points.push_back(Point(-4, 0, 50));
 	points.push_back(Point(12, 0, 50));
 	points.push_back(Point(12, 0, -2));
 	Polygon *floor = new Polygon(points, Color(0, 255, 0));
 	material = new Checker(floor);
-	//material = new Quilt(material);
+	material = new Quilt(material);
 	material = new Phong(material, &scene);
-	//material = new Reflection(material, &scene, 0.5);
+	material = new Reflection(material, &scene, 0.1);
 	scene.add(material);
 	
+	// This is the star
 	points.clear();
 	points.push_back(Point(-3.94528, 5.141731, 5.0));
 	points.push_back(Point(-4.03571, 5.790754, 5.0));
@@ -68,44 +75,57 @@ int main()
 	points.push_back(Point(-3.72288, 4.526534, 5.0));
 	points.push_back(Point(-4.36808, 4.64109, 5.0));
 	Polygon *star = new Polygon(points, Color(255, 255, 0));
-	//material = new Reflection(star, &scene, 1.0);
-	//material = new Phong(material, &scene);
+	material = new Reflection(star, &scene, 1.0);
+	material = new Phong(material, &scene);
 	material = new Phong(star, &scene);
-	//scene.add(material);
+	scene.add(material);
 	
+	//---------- Lighting definition
+	
+	// This is the light that matches the Turner-Whitted scene
 	Light light1(Point(1, 45, 0), Color(255,255,255), &scene);
-	scene.addLight(light1);
+	//scene.addLight(light1);
 	
+	// A reddish light in the upper left behind the view plane
 	Light light2(Point(-2, 10, -6), Color(255,64,64), &scene);
-	//scene.addLight(light2);
+	scene.addLight(light2);
 	
+	// A greenish light in the upper right just out of view
 	Light light3(Point(5, 5, 0), Color(64,255,64), &scene);
-	//scene.addLight(light3);
+	scene.addLight(light3);
 	
+	// a blueish light in the lower left
 	Light light4(Point(-5, 2, 5), Color(64,64,255), &scene);
-	//scene.addLight(light4);
+	scene.addLight(light4);
 	
 	cout << "Complete" << endl;
 	cout << "Creating camera...";
 	flush(cout);
 	
+	//---------- Camera definition
+	
+	// Camera
 	Point origin(0.0, 3.0, -6.0);
 	Point lookAt(0.0, 3.0, 5.0);
 	Camera *camera = new BasicCamera(&scene, origin, lookAt);
 	//camera = new WardToneCamera(camera, 1);
-	camera = new ReinhardToneCamera(camera, 1);
-	camera = new SuperSampleCamera(camera, 4);
+	camera = new ReinhardToneCamera(camera, 100);
+	camera = new SuperSampleCamera(camera, 2);
 	
 	cout << "Complete" << endl;
 	cout << "Rendering image...";
 	flush(cout);
 
-	int width = 640;
-	int height = 480;
-	//width = 1024;
-	//height = 768;
+	//---------- Rendering parameters
+	
+	// Output size
+	int width = 1024;
+	int height = 768;
+	//width = 640;
+	//height = 480;
 	Pixel* buffer = new Pixel[width * height];
 	
+	// Actual rendering
 	camera->getImage(width, height, buffer);
 	delete camera;
 	
@@ -113,12 +133,14 @@ int main()
 	cout << "Created Magick++ image...";
 	flush(cout);
 	
+	// Create a Magick++
 	Magick::Image image(width, height, "RGB", MagickLib::CharPixel, buffer);
 	
 	cout << "Complete" << endl;
 	cout << "Writing output...";
 	flush(cout);
 	
+	// Write the data to a png
 	image.write("image.png");
 	
 	delete[] buffer;
